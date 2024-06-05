838a4ff931d979ecf590be1728082f6cd94bf688
Anish Sahani <122989638+AnishSahani@users.noreply.github.com>
2024-06-05 07:47:11 +0000

    datatype

---
+++ 01_basic/datatype.js
@@ -0,0 +1,12 @@
+ "use strict";   //  treat all js code as newer version
+
+ //alert(3+3)   // we are using node js, not wed browser
+
+ console.log(3+3);   console.log("Hello world")
+
+
+ // number 2 to power 53
+ // bigint
+ // boolean true or false
+ //null  => standalone value
+ // undefined
