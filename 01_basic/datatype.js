eea04e500435b6349dca7652288ccf32000d3d82
Anish Sahani <122989638+AnishSahani@users.noreply.github.com>
2024-06-07 07:39:56 +0000

    commit

--- 01_basic/datatype.js
+++ 01_basic/datatype.js
@@ -10,3 +10,47 @@
  // boolean true or false
  //null  => standalone value
  // undefined
+
+
+
+ // primitive datatypes
+
+//  7 types: Strings,Number,Boolean,null,undefined,BigInt,Symbol
+
+//reference type(Non primitive type)
+
+// Array,Objects,Functions
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//   Stack(Primitive),   heap(Non Primitive)
+
+let myName="Anish Sahani"
+
+let anotherName=myName
+
+console.log(anotherName)
+
+anotherName="Akhilesh Sahani"
+
+console.log(myName)
+console.log(anotherName)
+
+
+let userone={
+    Name:"Binod",
+    Age:34
+    
+
+}
+
+let usertwo=userone
+
+// console.log(usertwo)
+
+usertwo.Name="Sachivji"
+usertwo.Age=26
+
+console.log(userone)
+console.log(usertwo)
