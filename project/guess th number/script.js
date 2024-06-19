7f9b52c44ac357f8eec5ce8ac1d33c9423f2ea6f
Anish Sahani <122989638+AnishSahani@users.noreply.github.com>
2024-06-19 09:37:29 +0000

    guessbthe number

--- project/guess th number/script.js
+++ project/guess th number/script.js
@@ -10,7 +10,7 @@ const startOver=document.querySelector('.result')
 const p=document.createElement('p')
 
 let prevGuess=[]
-let numguess=1
+let numGuess=1
 
 let playGame=true;
 
@@ -67,29 +67,27 @@ function displayMessage(message){
     lowOrHigh.innerHTML=`<h3>${message}</h3>`
 }
 
-function endGame(){
-       userInput.value=''
-       userInput.setAttribute('disabled','')
-       p.classList.add('button')
-       p.innerHTML=`<h2 id="newGame">Start New Game</h2>`
-       startOver.appendChild(p)
-       playGame=false
-       startGame()
-}
-
-function startGame(){
-    const newGameButton=document.querySelector('#newGame')
-
-    newGameButton.addEventListener('click',function(e){
-        randomNumber=parseInt(Math.random()*100+1)
-
-        prevGuess=[]
-        numguess=1
-        guessSlot.innerHTML=''
-        remaining.innerHTML=`${11-numguess}`
-        userInput.removeAttribute('disabled')
-        startOver.removeChild(p)
-
-        playGame=true
+function endGame() {
+    userInput.value = '';
+    userInput.setAttribute('disabled', '');
+    p.classList.add('button');
+    p.innerHTML = `<h2 id="newGame">Start new Game</h2>`;
+    startOver.appendChild(p);
+    playGame = false;
+    newGame();
+  }
+  
+  function newGame() {
+    const newGameButton = document.querySelector('#newGame');
+    newGameButton.addEventListener('click', function (e) {
+      randomNumber = parseInt(Math.random() * 100 + 1);
+      prevGuess = [];
+      numGuess = 1;
+      guessSlot.innerHTML = '';
+      remaining.innerHTML = `${11 - numGuess} `;
+      userInput.removeAttribute('disabled');
+      startOver.removeChild(p);
+  
+      playGame = true;
     })
 }
